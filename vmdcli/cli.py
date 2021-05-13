b397dd1890562f88b961ba9191cc91b80d993344
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2021-05-13 17:11:55 +0200

    Fix #1 and #2

--- vmdcli/cli.py
+++ vmdcli/cli.py
@@ -13,6 +13,10 @@ BASE_URL="https://vitemadose.gitlab.io/vitemadose"
 AVAILABLE_DAYS_CHOICES = ["1", "2", "7", "28", "49"]
 TZ = "Europe/Paris"
 
+headers = {
+    'User-Agent': f'vitemadose-cli v0.1.1',
+}
+
 logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -23,6 +27,12 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)  # A bit too verbose
 @click.command()
 @click.option(
     '--verbose',
+    help='Enable verbose mode',
+    default=False,
+    is_flag=True)
+@click.option(
+    '--chrono',
+    help='Only look for "chronodoses"',
     default=False,
     is_flag=True)
 @click.option(
@@ -38,13 +48,18 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)  # A bit too verbose
     '--pbtoken',
     help='Pushbullet token to send a notification',
     required=False)
-def main(verbose: bool, days:str, dept:str, pbtoken: str):
+def main(verbose: bool, chrono: bool, days:str, dept:str, pbtoken: str):
     """Look for available appointment(s) in the next X days in your departement.
     """
     if verbose:
         logger.setLevel(logging.DEBUG)
-    logger.info(f"Looking for available appointements in departement {dept} in the next {days} days...")
-    r = requests.get(f"{BASE_URL}/{dept}.json")
+    if not chrono:
+        logger.info(f"Looking for available appointements in departement {dept} in the next {days} days...")
+        _looking_period = f'{days}_days'
+    else:
+        logger.info(f"Looking for available appointements in departement {dept} for 'chronodoses'")
+        _looking_period = 'chronodose'
+    r = requests.get(f"{BASE_URL}/{dept}.json", headers=headers)
     if r.status_code == 404:
         # Easy one to understand
         logger.error("Invalid departement number")
@@ -64,20 +79,23 @@ def main(verbose: bool, days:str, dept:str, pbtoken: str):
     delta = datetime.now(timezone(TZ))-last_update
     logger.info(f"Last data update: {humanize.naturaldelta(delta)}")
     for centre in data.get("centres_disponibles", []):
-        as_for_period = int(centre.get('appointment_schedules', {}).get(f'{days}_days'))
-        if as_for_period > 0:
-            logger.info(f"{centre['nom']}: {as_for_period} available appointements in the next {days} days")
-            logger.info(f"  > {centre['url']}")
-            logger.info(f"  > Vaccins proposés: {','.join(centre.get('vaccine_type', []))}")
-            logger.info(f"  > Type d'établissement: {centre.get('type')}")
-            logger.debug(f"  > Metadata: {json.dumps(centre.get('metadata'), indent=4)}")
-            if pbtoken:
-                pb = Pushbullet(pbtoken)
-                pb.push_link(
-                    f"{centre['nom']}: {as_for_period} available appointements in the next {days} days",
-                    centre['url']
-                )
-                logger.debug("Pushbullet notification sent")
+        for app_sch in centre.get('appointment_schedules', []):
+            if app_sch.get('name') == _looking_period:
+                nb_slots = app_sch.get('total', 0)
+                if nb_slots > 0:
+                    if not chrono:
+                        _title = f"{centre['nom']}: {nb_slots} available appointements in the next {days} days"
+                    else:
+                        _title = f"{centre['nom']}: {nb_slots} 'chronodoses' availables"
+                    logger.info(_title)
+                    logger.info(f"  > {centre['url']}")
+                    logger.info(f"  > Vaccins proposés: {','.join(centre.get('vaccine_type', []))}")
+                    logger.info(f"  > Type d'établissement: {centre.get('type')}")
+                    logger.debug(f"  > Metadata: {json.dumps(centre.get('metadata'), indent=4)}")
+                    if pbtoken:
+                        pb = Pushbullet(pbtoken)
+                        pb.push_link(_title, centre['url'])
+                        logger.debug("Pushbullet notification sent")
         else:
             logger.debug(f"{centre['nom']}: no available appointment")
     return 0
